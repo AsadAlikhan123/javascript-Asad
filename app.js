49a7e7f16207dacd055fcfe7f3bd32bfeffbb2b3
AsadAlikhan123 <154706858+AsadAlikhan123@users.noreply.github.com>
2024-03-17 21:39:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let num1=+prompt("Enter Your Number");
+let num2=+prompt("Enter Your Number");
+
+let sum = num1+num2;
+let mun = num1-num2;
+let multi = num1*num2;
+let divid = num1/num2;
+let modul = num1%num2;
+
+document.write(num1," sum of ", num2,  " and totla value ",sum ,"<br>");
+
+document.write(num1," mul of ", num2,  " and totla value ",mun ,"<br>");
+
+document.write(num1," Multiplay of ", num2,  " and totla value ",multi,"<br>");
+
+document.write(num1," devided of ", num2,  " and totla value ",divid,"<br>");
+
+document.write(num1," modul of ", num2,  " and totla value ",modul,"<br>");
+
+
+let usertablenumber = +prompt ("Enter Your table Number")
+
+document.write(usertablenumber,"=", usertablenumber*1)
+document.write(usertablenumber,"=", usertablenumber*1)
+document.write(usertablenumber,"=", usertablenumber*1)
